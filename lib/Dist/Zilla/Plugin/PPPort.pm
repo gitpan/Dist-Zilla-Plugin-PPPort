7653f6a920895ee34f7a76780c2ed594e358156f
Leon Timmermans <fawaka@gmail.com>
2014-04-25 22:14:36 +0000

    Import of LEONT/Dist-Zilla-Plugin-PPPort-0.007 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-PPPort
    gitpan-cpan-version:      0.007
    gitpan-cpan-path:         LEONT/Dist-Zilla-Plugin-PPPort-0.007.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/PPPort.pm
+++ lib/Dist/Zilla/Plugin/PPPort.pm
@@ -1,37 +1,59 @@
 package Dist::Zilla::Plugin::PPPort;
-$Dist::Zilla::Plugin::PPPort::VERSION = '0.006';
-use 5.008;
+$Dist::Zilla::Plugin::PPPort::VERSION = '0.007';
 use Moose;
-with qw/Dist::Zilla::Role::FileGatherer/;
+with qw/Dist::Zilla::Role::FileGatherer Dist::Zilla::Role::PrereqSource/;
+use Moose::Util::TypeConstraints 'enum';
 use MooseX::Types::Perl qw(StrictVersionStr);
 use MooseX::Types::Stringlike 'Stringlike';
-use Devel::PPPort;
+use Devel::PPPort 3.23;
+use File::Spec::Functions 'catdir';
 
-my $content;
-{
-	local *PPPORT_FILE;
-	open PPPORT_FILE, '>', \$content or confess "Couldn't open scalar filehandle";
-	Devel::PPPort::WriteFile("&=Dist::Zilla::Plugin::PPPort::PPPORT_FILE");
-	close PPPORT_FILE;
-}
+has style => (
+	is  => 'ro',
+	isa => enum(['MakeMaker', 'ModuleBuild']),
+	default => 'MakeMaker',
+);
 
 has filename => (
 	is      => 'ro',
 	isa     => Stringlike,
+	lazy    => 1,
 	coerce  => 1,
-	default => 'ppport.h',
+	default => sub {
+		my $self = shift;
+		if ($self->style eq 'MakeMaker') {
+			return 'ppport.h';
+		}
+		elsif ($self->style eq 'ModuleBuild') {
+			my @module_parts = split /-/, $self->zilla->name;
+			return catdir('lib', @module_parts[0 .. $#module_parts - 1], 'ppport.h');
+		}
+		else {
+			confess 'Invalid style for XS file generation';
+		}
+	}
 );
 
 has version => (
 	is      => 'ro',
 	isa     => StrictVersionStr,
-	default => 0,
+	default => '3.23',
 );
 
 sub gather_files {
 	my $self = shift;
-	Devel::PPPort->VERSION($self->version) if $self->version;
-	$self->add_file(Dist::Zilla::File::InMemory->new(name => $self->filename, content => $content, encoding => 'ascii'));
+	Devel::PPPort->VERSION($self->version);
+	$self->add_file(Dist::Zilla::File::InMemory->new(
+		name => $self->filename,
+		content => Devel::PPPort::GetFileContents($self->filename),
+		encoding => 'ascii',
+	));
+	return;
+}
+
+sub register_prereqs {
+    my $self = shift;
+    $self->zilla->register_prereqs({ phase => 'develop' }, 'Devel::PPPort' => $self->version);
 	return;
 }
 
@@ -55,7 +77,7 @@ Dist::Zilla::Plugin::PPPort - PPPort for Dist::Zilla
 
 =head1 VERSION
 
-version 0.006
+version 0.007
 
 =head1 SYNOPSIS
 
@@ -68,7 +90,22 @@ In your dist.ini
 
 This module adds a PPPort file to your distribution. By default it's called C<ppport.h>, but you can name differently.
 
+=head1 ATTRIBUTES
+
+=head2 style
+
+This affects the default value for the C<filename> attribute. It must be either C<MakeMaker> or C<ModuleBuild>, the former being the default.
+
+=head2 filename
+
+The filename of the ppport file. It defaults to F<ppport.h> if C<style> is C<MakeMaker>, and something module specific if C<style> is C<Module::Build>.
+
+=head2 version
+
+This describes the minimal version of Devel::PPPort required for this module. It currently defaults to C<3.23>.
+
 =for Pod::Coverage gather_files
+register_prereqs
 =end
 
 =head1 AUTHOR
--- t/basic.t
+++ t/basic.t
@@ -7,16 +7,48 @@ use Test::More 0.88;
 use Test::DZil;
 use Path::Class;
 
-my $tzil = Builder->from_config(
-  { dist_root => 'corpus/' },
-  { },
-);
+{
+	my $tzil = Builder->from_config(
+	  { dist_root => 'corpus/' },
+	  {
+		  add_files => {
+			  'source/dist.ini' => simple_ini(
+				  qw/@Basic PkgVersion PPPort/
+			  ),
+		  },
+	  },
+	);
 
-$tzil->build;
+	$tzil->build;
 
-my $dir = dir($tzil->tempdir, 'build');
+	my $dir = dir($tzil->tempdir, 'build');
 
-ok -e $dir->file('ppport.h');
-ok -s $dir->file('ppport.h');
+	ok -e $dir->file('ppport.h');
+	ok -s $dir->file('ppport.h');
+}
+
+{
+	my $tzil = Builder->from_config(
+	  {
+		  dist_root => 'corpus/',
+	  },
+	  {
+		  add_files => {
+			  'source/dist.ini' => simple_ini(
+				  { name => 'Foo-Bar' },
+				  qw/@Basic PkgVersion/,
+				  [ PPPort => { style => 'ModuleBuild' } ],
+			  ),
+		  },
+	  },
+	);
+
+	$tzil->build;
+
+	my $dir = dir($tzil->tempdir, 'build');
+
+	ok -e $dir->file('lib/Foo/ppport.h');
+	ok -s $dir->file('lib/Foo/ppport.h');
+}
 
 done_testing;
